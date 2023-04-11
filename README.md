78de5bd7cf5dc34c0d0ceb849447f450b812d108
Michael MacCuaig <mmaccuaig@geodesys.com>
2023-04-11 14:13:43 +0100

    Init Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# OpenAI API Interaction Extension for Visual Studio Code
+
+This extension allows you to interact with the OpenAI Chat API directly within Visual Studio Code. It uses the contents of the current file as the prompt for the OpenAI API and displays the response in an output channel called OpenAI Coder.
+
+## Features
+
+- Send the contents of the current file as a prompt to the OpenAI API.
+- Display the API response in an Output channel.
+
+## Requirements
+
+- An OpenAI API key is required to use this extension. If you don't have an API key, you can obtain one by signing up at [https://beta.openai.com/signup](https://beta.openai.com/signup).
+
+## Getting Started
+
+1. Install the extension from the Visual Studio Code marketplace or by using the `.vsix` file provided in the project folder.
+
+2. Open a file in Visual Studio Code.
+
+3. Press `Ctrl+Shift+P` or `Cmd+Shift+P` to open the Command Palette.
+
+4. Type `OpenAI API Interaction` in the Command Palette and press Enter.
+
+5. If you haven't entered your API key yet, you will be prompted to do so. The extension will store the API key for future use.
+
+6. The contents of the current file will be sent as a prompt to the OpenAI API, and the response will be displayed in an information message.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+- `openaiAPI.apiKey`: Your OpenAI API key. This setting is used to authenticate with the OpenAI API.
+
+## Known Issues
+
+None.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of the OpenAI API Interaction extension.
+
+## Contributing
+
+If you'd like to contribute to the development of this extension, please submit issues and/or pull requests on the GitHub repository.
+
+## License
+
+This extension is released under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ extension.js
@@ -0,0 +1,99 @@
+const vscode = require('vscode');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+async function interactWithOpenAI() {
+    const config = vscode.workspace.getConfiguration('openaiAPI');
+	let apiKey = config.get('apiKey');
+	let openaiAPIoutput = vscode.window.createOutputChannel("openaiAPI");
+
+    if (!apiKey) {
+        apiKey = await vscode.window.showInputBox({
+            prompt: 'Enter your OpenAI API key:',
+        });
+
+        if (!apiKey) {
+            vscode.window.showErrorMessage('API key not provided.');
+            return;
+        }
+
+        await config.update('apiKey', apiKey, vscode.ConfigurationTarget.Global);
+    }
+
+    const activeEditor = vscode.window.activeTextEditor;
+	if (!activeEditor) {
+		vscode.window.showErrorMessage('No active editor found.');
+		return;
+	}
+
+    const document = activeEditor.document;
+    const content = document.getText();
+
+	if (!content) {
+		vscode.window.showErrorMessage('The current file is empty.');
+		return;
+	}
+
+	const prompt = "Optimize the following code: " + content;
+
+    try {
+
+		openaiAPIoutput.appendLine("Prompt sent.");
+
+        const response = await axios.post(
+            'https://api.openai.com/v1/chat/completions',
+			{
+				model: "gpt-3.5-turbo",
+                messages: [
+                    { role: "system", content: "You are a helpful assistant." },
+                    { role: "user", content: prompt }
+                ]
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${apiKey}`,
+                },
+            }
+		);
+
+		let completion = response.data.choices[0].message.content;
+		
+		const workspaceFolder = path.dirname(document.uri.fsPath);
+		const aiResponsesFolder = path.join(workspaceFolder, 'ai_responses');
+		const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+		const responseFilename = `response_${timestamp}.md`;
+		const responsePath = path.join(aiResponsesFolder, responseFilename);
+
+		openaiAPIoutput.appendLine("Response received.");
+
+		try {
+			fs.mkdirSync(aiResponsesFolder, { recursive: true });
+			fs.writeFileSync(responsePath, completion, 'utf-8');
+			vscode.window.showInformationMessage(`OpenAI Response saved to: ${responsePath}`);
+
+			const responseUri = vscode.Uri.file(responsePath);
+            await vscode.commands.executeCommand('vscode.open', responseUri);
+            await vscode.commands.executeCommand('markdown.showPreview', responseUri);
+		} catch (err) {
+			vscode.window.showErrorMessage(`Error saving OpenAI Response: ${err.message}`);
+		}
+    } catch (error) {
+		vscode.window.showErrorMessage('Error interacting with the OpenAI API.');
+        openaiAPIoutput.appendLine(error);
+		
+    }
+}
+
+function activate(context) {
+    let disposable = vscode.commands.registerCommand('extension.interactWithOpenAI', interactWithOpenAI);
+    context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate,
+};
